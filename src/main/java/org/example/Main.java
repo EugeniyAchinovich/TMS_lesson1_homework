78f0bde1b59ee58426226861bdf5bf8bf3d325f7
Eugene Achinovich <e.achinovitch@yandex.ru>
2023-08-27 14:39:33 +0300

    Вывод результатов операций теперь с округлением до 2 знаков

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@
 
 package org.example;
 
+import java.text.DecimalFormat;
 
 public class Main {
     // Сложение двух вещественных чисел
@@ -21,10 +22,13 @@ public class Main {
         return a / b;
     }
 
+    // Формат округления вещественных чисел до двух знаков после запятой
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
     public static void main(String[] args) {
-        System.out.println("6.7 + 2.3 = " + sum(6.7, 2.3));
-        System.out.println("0.45 - 0.15 = " + dec(0.45, 0.15));
-        System.out.println("0.1 * 0.1 = " + mul(0.1, 0.1));
-        System.out.println("100 / 300 = " + div(100, 300));
+        System.out.println("6.7 + 2.3 = " + df.format(sum(6.7, 2.3)));
+        System.out.println("0.45 - 0.15 = " + df.format(dec(0.45, 0.15)));
+        System.out.println("0.1 * 0.1 = " + df.format(mul(0.1, 0.1)));
+        System.out.println("100 / 300 = " + df.format(div(100, 300)));
     }
 }
