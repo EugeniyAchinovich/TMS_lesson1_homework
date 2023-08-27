7aa0cff530e2b450a4f271488dcd50ecb56bf4bb
Eugene Achinovich <e.achinovitch@yandex.ru>
2023-08-27 14:08:05 +0300

    Добавлены функции mul() и div() - умножение и деление

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+
     public static int sum(int a, int b) {
         return a + b;
     }
@@ -9,8 +10,18 @@ public class Main {
         return a - b;
     }
 
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static float div(float a, float b) {
+        return a / b;
+    }
+
     public static void main(String[] args) {
         System.out.println("2 + 7 = " + sum(2, 7));
         System.out.println("9 - 6 = " + dec(9, 6));
+        System.out.println("5 * 5 = " + mul(5, 5));
+        System.out.println("30 / 4 = " + div(30, 4));
     }
 }
