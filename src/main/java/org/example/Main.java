e65f21499c0def3c751ebd60ba5e390112c89326
Eugene Achinovich <e.achinovitch@yandex.ru>
2023-08-27 14:03:19 +0300

    Добавлены функции sum() и dec() - сложения и вычитания

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class Main {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int dec(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("2 + 7 = " + sum(2, 7));
+        System.out.println("9 - 6 = " + dec(9, 6));
     }
 }
